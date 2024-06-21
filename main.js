ba81d248476bcb77583486783712ceebcd121ac7
Trần Thiện Thanh <kurotsmile@gmail.com>
2024-06-21 20:17:50 +0700

    update html
    update html

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ var id_table="pi_work";
 var index_cur=0;
 var index_cur_pin=1;
 var length_pi=500;
+var position_btn_add='right';
 
 $(document).ready(function(){
     load_list();
@@ -253,15 +254,22 @@ function show_setting(){
     var html='<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-gear-fill" viewBox="0 0 16 16"><path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z"/></svg><br/><h3>Setting</h3>';
     html+='<div class="row">';
         html+='<div class="col-8">Button to mark the list of objects</div>';
-        html+='<div class="col-4">Thanh</div>';
+        html+='<div class="col-4">';
+            html+='<select id="pos_btn_add" class="form-control">';
+                html+='<option value="right">Right</option>';
+                html+='<option value="left">Left</option>';
+                html+='<option vlaue="none">None</option>';
+            html+='</select>';
+        html+='</div>';
     html+='</div>';
     Swal.fire({html:html,showCancelButton: true}).then((result) => {
         if (result.isConfirmed) {
-          Swal.fire({
-            title: "Save setting!",
-            text: "Update installed successfully!",
-            icon: "success"
-          });
+            alert($("#pos_btn_add").val());
+            Swal.fire({
+                title: "Save setting!",
+                text: "Update installed successfully!",
+                icon: "success"
+            });
         }
-      });
+    });
 }
