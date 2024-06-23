2b7363839f029bb00f02441791223939a8666f56
Trần Thiện Thanh <kurotsmile@gmail.com>
2024-06-24 00:00:22 +0700

    Update setting
    Update setting

--- main.js
+++ main.js
@@ -3,15 +3,17 @@ var list_pin=["🤍","🔴","🧡","💙","💜","💚"];
 var list_pin_id=["none","check","alert","info","memory","success"];
 var list_total=[200,350,400,450,500];
 
-var id_table="pi_work";
+var id_table="Work";
 var index_cur=0;
 var index_cur_pin=1;
 var length_pi=500;
 var position_btn_add='right';
+var setting_show_timer='show';
 
 $(document).ready(function(){
     $("#menu_info").hide();
     if(localStorage.getItem("pos_btn_add")!=null) position_btn_add=localStorage.getItem("pos_btn_add");
+    if(localStorage.getItem("setting_show_timer")!=null) setting_show_timer=localStorage.getItem("setting_show_timer");
     load_list();
     load_pi();
 });
@@ -39,7 +41,9 @@ function load_pi(){
             s_class="sel "+list_pin_id[index_p];
         }
 
-        html_pi+="<div id='box_"+pi+"' class='box "+s_class+"' index='"+pi+"'><span class='name'>"+pi+"</span> <span id='timer_"+pi+"' class='timer'>None ☢</span></div>";
+        html_pi+="<div id='box_"+pi+"' class='box "+s_class+"' index='"+pi+"'><span class='name'>"+pi+"</span>";
+        if(setting_show_timer=="show") html_pi+="<span id='timer_"+pi+"' class='timer'>None ☢</span>";
+        html_pi+="</div>";
     }
     html_pi+='</div>';
 
@@ -47,13 +51,15 @@ function load_pi(){
 
     $("#pi").html(html_pi);
 
-    for(var pi=1;pi<=length_pi;pi++){
-        const endTime = localStorage.getItem(id_table+"_pi_"+pi+"_timer");
-        if (endTime){
-            startCountdown(pi,new Date(parseInt(endTime)));
+    if(setting_show_timer=="show"){
+        for(var pi=1;pi<=length_pi;pi++){
+            const endTime = localStorage.getItem(id_table+"_pi_"+pi+"_timer");
+            if (endTime){
+                startCountdown(pi,new Date(parseInt(endTime)));
+            }
         }
     }
-
+ 
     $(".box").click(function(){
         var index=$(this).attr("index");
         click_box_obj(index);
@@ -292,8 +298,11 @@ function startCountdown(index, existingEndTime = null) {
 
 function show_setting(){
     var html='<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-gear-fill" viewBox="0 0 16 16"><path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z"/></svg><br/><h3>Setting</h3>';
-    html+='<div class="row">';
-        html+='<div class="col-8">Button to mark the list of objects</div>';
+    html+='<div class="row text-left">';
+        html+='<div class="col-8 text-left">';
+            html+='<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar2-plus-fill" viewBox="0 0 16 16"><path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M2 3.5v1c0 .276.244.5.545.5h10.91c.3 0 .545-.224.545-.5v-1c0-.276-.244-.5-.546-.5H2.545c-.3 0-.545.224-.545.5m6.5 5a.5.5 0 0 0-1 0V10H6a.5.5 0 0 0 0 1h1.5v1.5a.5.5 0 0 0 1 0V11H10a.5.5 0 0 0 0-1H8.5z"/></svg> ';
+            html+='Button to mark the list of objects';
+        html+='</div>';
         html+='<div class="col-4">';
             html+='<select id="pos_btn_add" class="form-control">';
                 html+='<option value="right" '+(position_btn_add==='right' ? ' selected' : '') +'>Right</option>';
@@ -302,11 +311,38 @@ function show_setting(){
             html+='</select>';
         html+='</div>';
     html+='</div>';
+
+    html+='<div class="row">';
+        html+='<div class="col-8 text-left">';
+            html+='<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-circle-fill" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814z"/></svg> Show Timer';
+        html+='</div>';
+        html+='<div class="col-4">';
+            html+='<select id="setting_show_timer" class="form-control">';
+                html+='<option value="show" '+(setting_show_timer==='show' ? ' selected' : '') +'>Show</option>';
+                html+='<option value="hide" '+(setting_show_timer==='hide' ? ' selected' : '') +'>Hide</option>';
+            html+='</select>';
+        html+='</div>';
+    html+='</div>';
+
+    html+='<div class="row">';
+        html+='<div class="col-8 text-left">';
+            html+='<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-grid-3x3-gap-fill" viewBox="0 0 16 16"><path d="M1 2a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1zm5 0a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1zm5 0a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1zM1 7a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1zm5 0a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1zm5 0a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1zM1 12a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1zm5 0a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1zm5 0a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1z"/></svg> Length';
+        html+='</div>';
+        html+='<div class="col-4">';
+            html+='<input class="form-control" id="length_pi" value="'+length_pi+'"/>';
+        html+='</div>';
+    html+='</div>';
+
     Swal.fire({html:html,showCancelButton: true}).then((result) => {
         if (result.isConfirmed) {
             position_btn_add=$("#pos_btn_add").val();
+            setting_show_timer=$("#setting_show_timer").val();
+            length_pi=parseInt($("#length_pi").val());
+
             localStorage.setItem('pos_btn_add',position_btn_add);
-            
+            localStorage.setItem("setting_show_timer",setting_show_timer);
+            localStorage.setItem(id_table+"_lenth_app",length_pi);
+
             Swal.fire({
                 title: "Save setting!",
                 text: "Update installed successfully!",
@@ -320,9 +356,13 @@ function show_setting(){
 }
 
 function edit_app(){
+    var s_val='';
+    if(localStorage.getItem("app_id_"+index_cur)!=null) s_val=localStorage.getItem("app_id_"+index_cur);
+
     Swal.fire({
         title:"Enter the app's package id for ("+index_cur+")",
-        input:"text",
+        inputValue: s_val,
+        input: "text",
         showCancelButton: true,
         preConfirm: async (app_id) => {
            localStorage.setItem("app_id_"+index_cur,app_id);
